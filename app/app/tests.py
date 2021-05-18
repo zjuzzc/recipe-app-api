2518d6f2ed7aeb693784ce7eea0ba0a5190d279f
zzc-quantvortex <zzc@quantvortex.com>
2021-05-18 11:27:53 -0700

    unit tests

---
+++ app/app/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from app.calc import add, subtract
+
+
+class CalcTest(TestCase):
+    def test_add_numbers(self):
+        self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_numbers(self):
+        self.assertEqual(subtract(4, 12), 8)
